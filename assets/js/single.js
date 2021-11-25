08e9c87ba4dae1c531f591f932cc3621e590556a
AdamCampeau <adamcampeau3@gmail.com>
2021-11-25 10:41:18 -0500

    added localStorage framework

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,12 @@
-var x = document.getElementById("myTextarea");
 
-//(api.openweathermap.org/data/2.5/weather?q={city name}&appid=a9be3d064e53eb67f8c76693caefab55)
+//(api.openweathermap.org/data/2.5/weather?q=cityName&appid=a9be3d064e53eb67f8c76693caefab55)
+
+$(".searchBtn").on("click", function () {
+    console.log("search button clicked");
+    var city = $(this).attr("data-name");
+    var task = $(this).siblings('textarea').val()
+  
+    window.localStorage.setItem(city);
+    getTasks()
+  
+  })
