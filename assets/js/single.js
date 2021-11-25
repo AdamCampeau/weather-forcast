2b4729870ee4c34f7401c8acf5619f024dd8cff5
AdamCampeau <adamcampeau3@gmail.com>
2021-11-25 10:12:26 -0500

    search button added

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,3 @@
 var x = document.getElementById("myTextarea");
 
-// fetch(    api.openweathermap.org/data/2.5/weather?q={city name}&appid=a9be3d064e53eb67f8c76693caefab55)
+//(api.openweathermap.org/data/2.5/weather?q={city name}&appid=a9be3d064e53eb67f8c76693caefab55)
