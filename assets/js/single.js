d167fbdee9a1cff56c820f6729da7ce79f7238a1
AdamCampeau <adamcampeau3@gmail.com>
2021-11-26 14:31:24 -0500

    updated i loop

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,25 +1,44 @@
+// Search Button
+var 
+
+
 // City Weather API fetch
-let weather = {searchWeather(){
-        fetch("api.openweathermap.org/data/2.5/weather?q=" 
-        + city +
-        + lot +
-        + lon +
-        "&units=metric&appid=a9be3d064e53eb67f8c76693caefab55")
-        console.log(city)  
-        // pass coordinates to forecast api?
+var weather = {searchWeather(){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" 
+        + city +        
+        "&units=imperial&appid=a9be3d064e53eb67f8c76693caefab55");
+        //console.log(city);
+        
+    then(response = response.json())
+    // console.log(response)
+},
 
-    then(response => response.json());
-    console.log(response);
+// Coordinates from API
+var weatherCoord =
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" +lat + "&lon=" + lon + "&units=imperial&appid=a9be3d064e53eb67f8c76693caefab55");   
+    // console.log(lat);
+    // console'log(lon);
+}
 
-// forecast weather api fetch
-let forecast = {
-    searchForecast() {
 
-// dynamically create cards for 4 day forecast
-/*for (var i = 0; i <= 5; i++) {
-    console.log(i)
+// create 5 day forecast
+var weatherForecast = function(data) {
+    var dailyForecast=data.daily;
+    //loop through days while creating card elements
+    for (var i = 0; i <= 5; i++) {
+        var icon=dailyForecast[i].weather[0].icon;
+        var temp=dailyForecast[i].temp.day;
+        var wind=dailyForecast[i].wind_speed;
+        var humidity=dailyForecast[i].humidity;
+        var displayIcon=document.querySelector("#day-" + [i]);
+        displayIcon.src="https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        var displayTemp=document.querySelector("#")
+
+
+
     var card = $('<div>')
     card.addClass('col')*/
+
    
  
 // search button saves to local storage
@@ -31,23 +50,23 @@ $(".searchBtn").on("click", weather);
     getCity();
 }
 
-document.getElementByClass("theBtn").addEventListener("click", weather);
+/*document.getElementByClass("theBtn").addEventListener("click", weather);
 element.addEventListener("click", myFunction);
 element.addEventListener("cl
 
 function myFunction() {
   alert ("Hello World!");
-}
+}*/
 
 // retrieve search history in local storage
-  $(".savedCity").on("click", weather),
+  /*$(".savedCity").on("click", weather),
     //console.log("saved city clicked"),
     var city = $(this).attr("data-name",
     
    window.localStorage.setItem(city);
     getCity();
 
-  }
+  }*/
 
   /*function getCity() {
     var storage = window.localStorage.getItem(city);
