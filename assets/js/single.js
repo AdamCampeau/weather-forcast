6e3c32534f8a486c9942099c3b36489819c07421
AdamCampeau <adamcampeau3@gmail.com>
2021-11-28 12:05:22 -0500

    updated code, still not working

--- assets/js/single.js
+++ assets/js/single.js
@@ -5,44 +5,45 @@ $(document).ready(function(){
     $(".resultWeather").append("<p id=currentTemp></p>");
     $(".resultWeather").append("<p id=currentHumidity></p>");
     $(".resultWeather").append("<p id=currentWind></p>");
-    // $(".resultWeather").append("<p id=uvIndex></p>")
+    $(".resultWeather").append("<p id=uvIndex></p>");
+    $(".resultWeather").append("<p id=uvIndex></p>")
     $('#searchBtn').keypress(function(event){
         if (event.keyCode == 10 || event.keyCode == 13) 
             event.preventDefault();
             
       });
       apiCall();
-      // apiCallTwo();
-      
 })
 
 function apiCall() {
     var currentCity="Toronto";
+    var cityLat= 'response.coord.lat';
+    var cityLon = 'response.coord.lon';
     console.log(currentCity);
     console.log('anything')
     // get element by class
     // var currentCity=$("#cityName").val();
-    var cordUrl="https://api.openweathermap.org/data/2.5/onecall?" + cityLat + cityLon + "&appid=a9be3d064e53eb67f8c76693caefab55";
+    
     var cityUrl="https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55";
-    
-    
-    
+    var cordUrl="https://api.openweathermap.org/data/2.5/onecall?" + cityLat + cityLon + "&appid=a9be3d064e53eb67f8c76693caefab55";
+      
     
     $.ajax({
-        url: cityUrl,
+        url: cityUrl , cordUrl,
         method: "GET"
         }).then(function(response) {
-            console.log(response); 
+            console.log(response);
+            //$('#cityLat').text(cityLat);
+            //$('#cityLon').text(cityLon); 
             $('#currentCity').text(currentCity);       
             var forecastIconCode = response.weather[0].icon;
             var forecastIconUrl = "https://openweathermap.org/img/wn/"+forecastIconCode+".png";
             $('#forecastIcon').attr('src', forecastIconUrl);
             $('#currentTemp').text(response.main.temp);
             $('#currentHumidity').text(response.main.humidity);
-            $('#currentWind').text(response.main.wind);
-            $('.cityLat').text(response.coord.lat);
-            $('.cityLon').text(response.coord.lon);
-            
+            $('#currentWind').text(response.main.wind); 
+            $('#cityLat').text(response.coord.lat);
+            $('#cityLon').text(response.coord.lon);           
             
             console.log('iconBlock');
               
@@ -54,11 +55,16 @@ function apiCallTwo() {
         url: cordUrl,
         method: "GET"
         }).then(function(response) {
+            var dailyIconCode = response.weather[0].icon;
+            var dailyIconUrl="http://openweathermap.org/img/wn/"+ dailyIconCode +".png"
+            $('#dailyIcon').attr('src', dailyIconUrl);
             $('#uvIndex').text(response.current.uvi);
             $('#forescastId').text(response.daily.weather.icon);
             $('#forecastTemp').text(response.daily.temp);
             $('#forecastHumid').text(response.daily.humidity);
             $('#forecastWind').text(response.daily.wind_speed);
+    })
+}
 
     
 
