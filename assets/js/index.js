7bb2cb42093d7463dd4659f87d53d478e84487e5
AdamCampeau <adamcampeau3@gmail.com>
2021-12-16 20:17:37 -0500

    pseudo coding whats left to do

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,6 +16,22 @@ function getCurrentWeather(currentCity) {
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        currentCity.textContent = 'hello'
+        currentCity.textContent = ''
     })
+    
 }
+
+// parse data
+
+// main: {temp: 12.83, feels_like: 12.02, temp_min: 10.82, temp_max: 13.97, pressure: 1002, …}
+//name: "Toronto
+//visibility: 10000
+//weather: [{…}]
+//wind: {speed: 2.24, deg: 231, gust: 7.15}
+//clouds: {all: 40}
+
+// server response code
+//cod: 200
+
+//longitude and latitude needed for other API use for 5 Day forecaset
+//coord: {lon: -79.4163, lat: 43.7001
