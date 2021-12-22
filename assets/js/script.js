11caeb88222dda0a47cf7533be1d42851f9b2e40
AdamCampeau <adamcampeau3@gmail.com>
2021-12-21 20:44:25 -0500

    updated api data content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-const searchBtn = document.querySelector(".searchBtn")
-let cityContainer = document.querySelector("#currentCity")
-let currentTemp = document.getElementById('currentTemp')
-let weatherIcon = document.getElementById('weatherIcon')
-let feelsLike = document.getElementById('feelsLike')
-let currentHumidity = document.getElementById('currentHumidity')
-let tempMax = document.getElementById('tempMax')
-const 
+var searchBtn = document.querySelector(".searchBtn")
+var cityContainer = document.querySelector("#currentCity")
+var currentTemp = document.getElementById('currentTemp')
+var weatherIcon = document.getElementById('weatherIcon')
+var feelsLike = document.getElementById('feelsLike')
+var currentHumidity = document.getElementById('currentHumidity')
+var tempMax = document.getElementById('tempMax')
+var tempMin = document.getElementById('tempMin')
+
 
 // var cityUrl="https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55";
 
@@ -21,16 +22,15 @@ function getCurrentWeather(currentCity) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55")
     .then(response => response.json())
     .then(data => {
-        //console.log(data);
+        console.log(data);
         cityContainer.textContent = data.name
-        weatherIcon.textcontent = data.weather
+        weatherIcon.textcontent = data.weather.icon
         currentTemp.textContent = data.main.temp
         feelsLike.textContent = data.main.feels_like
         currentHumidity.textContent = data.main.humidity
         tempMax.textContent = data.main.tempMax
         tempMin.textContent = data.main.tempMin
-        // uvIndex.textContent =
-        console.log()
-        //uvIndex.textContent = data.main
     })
+    //  "http://openweathermap.org/img/wn/" + data.weather.icon + ".png";)
+    
 }
