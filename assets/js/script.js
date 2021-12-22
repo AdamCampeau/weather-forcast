1b8d38c0d0f0612ce1d5cc481b8c2730905f3dc2
AdamCampeau <adamcampeau3@gmail.com>
2021-12-21 19:50:29 -0500

    const and let refractor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-var searchBtn = document.querySelector(".searchBtn")
-var cityContainer = document.querySelector("#currentCity")
-var currentTemp = document.getElementById('currentTemp')
+const searchBtn = document.querySelector(".searchBtn")
+let cityContainer = document.querySelector("#currentCity")
+let currentTemp = document.getElementById('currentTemp')
+let weatherIcon = document.getElementById('weatherIcon')
+let feelsLike = document.getElementById('feelsLike')
+let currentHumidity = document.getElementById('currentHumidity')
+let tempMax = document.getElementById('tempMax')
+const 
 
 // var cityUrl="https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55";
 
@@ -16,8 +21,15 @@ function getCurrentWeather(currentCity) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         cityContainer.textContent = data.name
-        currentTemp.textContent = data.main.currentTemp
+        weatherIcon.textcontent = data.weather
+        currentTemp.textContent = data.main.temp
+        feelsLike.textContent = data.main.feels_like
+        currentHumidity.textContent = data.main.humidity
+        tempMax.textContent = data.main.tempMax
+        tempMin.textContent = data.main.tempMin
+        console.log()
+        //uvIndex.textContent = data.main
     })
 }
