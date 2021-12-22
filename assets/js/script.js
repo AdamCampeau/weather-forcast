343ac410bf04f44dabb56a187d18cdd0d6ef2164
AdamCampeau <adamcampeau3@gmail.com>
2021-12-21 22:37:29 -0500

    working on showing weather icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,14 +22,19 @@ function getCurrentWeather(currentCity) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55")
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         cityContainer.textContent = data.name
         weatherIcon.textcontent = data.weather.icon
         currentTemp.textContent = data.main.temp
+        console.log(currentTemp)
         feelsLike.textContent = data.main.feels_like
+        console.log(feelsLike)
         currentHumidity.textContent = data.main.humidity
-        tempMax.textContent = data.main.tempMax
-        tempMin.textContent = data.main.tempMin
+        console.log(currentHumidity)
+        tempMax.textContent = data.main.temp_max
+        console.log(tempMax)
+        tempMin.textContent = data.main.temp_min
+        console.log(tempMin)
     })
      // "http://openweathermap.org/img/wn/" + data.weather.icon + ".png"
     
