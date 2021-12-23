604cf123c9e2fafece8548915776c180b6ef543f
AdamCampeau <adamcampeau3@gmail.com>
2021-12-23 00:14:34 -0500

    uvi added

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var tempMin = document.getElementById('tempMin')
 
 
 // var cityUrl="https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55";
+// 5 day -- "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=a9be3d064e53eb67f8c76693caefab55"
 
 searchBtn.addEventListener("click", getCity)
 
@@ -22,22 +23,32 @@ function getCurrentWeather(currentCity) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55")
     .then(response => response.json())
     .then(data => {
+        var lat = data.coord.lat
+        var lon = data.coord.lon
+        getFiveDay(lat,lon)
         //console.log(data);
         cityContainer.textContent = data.name
-        //var weatherIcon = data.weather[0].icon
-        //var weatherIconURL = "http://openweathermap.org/img/wn/" + weatherIcon + ".png"
-        //$(".icon").html("<img src='" + weatherIconURL + "'>" )
         currentTemp.textContent = data.main.temp
-        console.log(currentTemp)
+        //console.log(currentTemp)
         feelsLike.textContent = data.main.feels_like
-        console.log(feelsLike)
+        //console.log(feelsLike)
         currentHumidity.textContent = data.main.humidity
-        console.log(currentHumidity)
+        //console.log(currentHumidity)
         tempMax.textContent = data.main.temp_max
-        console.log(tempMax)
+        //console.log(tempMax)
         tempMin.textContent = data.main.temp_min
-        console.log(tempMin)
+        //console.log(tempMin)
     })
      // "http://openweathermap.org/img/wn/" + data.weather.icon + ".png"
     
+}
+
+function getFiveDay(lat, lon) {
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon + "&appid=a9be3d064e53eb67f8c76693caefab55")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        uvIndex.textContent = data.current.uvi
+        console.log(uvIndex)
+    })
 }
