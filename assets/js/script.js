2dd27a7ccdf0b24420925d57c6e9610004f22de1
AdamCampeau <adamcampeau3@gmail.com>
2021-12-28 12:46:25 -0500

    weather icon, local storage, and 5 day now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,10 @@ var feelsLike = document.getElementById('feelsLike')
 var currentHumidity = document.getElementById('currentHumidity')
 var tempMax = document.getElementById('tempMax')
 var tempMin = document.getElementById('tempMin')
-var weeklyForecastContainer = document.querySelector('weekly-forecast-container')
+var weeklyForecastContainer = document.querySelector('#weekly-forecast-container')
+var uvIndex = document.getElementById('uvIndex')
+var icon = document.getElementById('displayIcon')
+var historyContainer = document.getElementById('savedCity')
 
 
 // var cityUrl="https://api.openweathermap.org/data/2.5/weather?q=" + currentCity +"&units=metric&appid=a9be3d064e53eb67f8c76693caefab55";
@@ -18,6 +21,36 @@ function getCity(e) {
     e.preventDefault()
     var city = document.querySelector('.city').value
     getCurrentWeather(city)
+    setHistory(city)
+}
+
+function setHistory(city) {
+    var storage = JSON.parse(localStorage.getItem('weatherHistory'))
+    if(storage === null) {
+        storage = []
+    }
+    storage.push(city)
+    localStorage.setItem('weatherHistory', JSON.stringify(storage))
+    createHistory()
+}
+
+createHistory()
+function createHistory() {
+    var storage = JSON.parse(localStorage.getItem('weatherHistory'))
+    if(storage === null) {
+        historyContainer.textContent = 'No Current Search History'
+    } else {
+        historyContainer.textContent = ''
+        for (var i = 0; i < storage.length; i++) {
+            var historyBtn = document.createElement('button')
+            historyBtn.textContent = storage[i]
+            historyContainer.append(historyBtn)
+
+            historyBtn.addEventListener('click', function(event) {
+                getCurrentWeather(event.target.textContent)
+            })
+        }
+    }
 }
 
 function getCurrentWeather(currentCity) {
@@ -30,7 +63,8 @@ function getCurrentWeather(currentCity) {
         
         //console.log(data);
         weatherIcon = data.weather[0].icon
-        displayIcon = "http://openweathermap.org/img/wn/" + weatherIcon + ".png"
+        iconLink = "http://openweathermap.org/img/wn/" + weatherIcon + ".png"
+        icon.setAttribute('src', iconLink)
 
         cityContainer.textContent = data.name
         
@@ -54,55 +88,61 @@ function getCurrentWeather(currentCity) {
 }
 
 function getFiveDay(lat, lon) {
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon + "&appid=a9be3d064e53eb67f8c76693caefab55")
+    weeklyForecastContainer.textContent = ''
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ lon + "&appid=a9be3d064e53eb67f8c76693caefab55&units=metric")
     .then(res => res.json())
     .then(data => {
         console.log(data);
         uvIndex.textContent = "UV Index: " + data.current.uvi
 
         if(uvIndex >= 7){
-            cityUvEl.classList = "badge bg-danger";
-            cityUvEl.textContent = uvIndex;
+            uvIndex.setAttribute("class","badge bg-danger");
+            
         }
 
-        else if(uvIndex < 7 && uvIndex > 2) {
-            cityUvEl.classList = "badge bg-warning";
-            cityUvEl.textContent = uvIndex;
+        else if(data.current.uvi < 7 && data.current.uvi > 2) {
+            uvIndex.setAttribute("class","badge bg-warning");
+            
 
         } 
         else {
-            cityUvEl.classList = "badge bg-success";
-            cityUvEl.textContent = uvIndex;
+            uvIndex.setAttribute("class","badge bg-success");
+            
         } 
    
-        for (var i = 0; i < 5; i++)
-        var card = document.createElement('div')
-        card.setAttribute('class','card')
-        weeklyForecastContainer.append(card)
+        for (var i = 0; i < 5; i++) {
+            var card = document.createElement('div')
+            card.setAttribute('class','card')
+            weeklyForecastContainer.append(card)
+    
+            var date = document.createElement('h3')
+            date.textContent = moment().add(i+1, 'days').format('dddd')
+            card.prepend(date)
 
-        var date = document.createElement('h3')
-        date.textContent = momement().add(i+1, 'days)').format('dddd')
-        card.prepend(date)
-
-        dailyTempHigh.textContent ="High of: " + data.daily[i].temp.max
-        console.log(dailyTempHigh)
-        card.append (dailyTempHigh)
-        
-        dailyTempLow.textContent="Low of: " + data.daily[i].temp.min
-        console.log(dailyTempLow)
-        card.append (dailyTempLow)
-
-        dailyHumidity.textContent=data.daily[i].humidity
-        console.log(dailyHumidity)
-        card.append (dailyHumidity)
-
-        dailyWind.textContent=data.daily[i].wind_speed + " Km/H"
-        console.log(dailyWind)
-        card.append (dailyWind)
-
-        dailyUvi.textContent=data.daily[i].uvi
-        console.log(dailyUvi)
-        card.append (dailyUvi)
+            var fiveDayIcon = document.createElement('img')
+            fiveDayIcon.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + ".png")
+            card.append(fiveDayIcon)
+    
+            var dailyTempHigh = document.createElement('p')
+            dailyTempHigh.textContent ="High of: " + data.daily[i].temp.max
+            console.log(dailyTempHigh)
+            card.append (dailyTempHigh)
+            
+            var dailyTempLow = document.createElement('p')
+            dailyTempLow.textContent="Low of: " + data.daily[i].temp.min
+            console.log(dailyTempLow)
+            card.append (dailyTempLow)
+    
+            var dailyHumidity = document.createElement('p')
+            dailyHumidity.textContent=data.daily[i].humidity
+            console.log(dailyHumidity)
+            card.append (dailyHumidity)
+    
+            var dailyWind = document.createElement('p')
+            dailyWind.textContent=data.daily[i].wind_speed + " Km/H"
+            console.log(dailyWind)
+            card.append (dailyWind)
+        }
 
     })
 }
